19c966682f1fb5d0fffe1737c060e258ce78e035
oliverrr <32515201+this-oliver@users.noreply.github.com>
2024-07-20 15:14:02 +0200

    patch: adds script to automate registry setup (#1)

--- registry/README.md
+++ registry/README.md
@@ -4,29 +4,41 @@
 
 The homelab uses [distribution](https://distribution.github.io/distribution/), an open-source docker registry, to store and manage docker images.
 
-## Getting Started
+## Usage
 
 Pre-requisites:
 
 - Docker
-- (Optional) Domain Name and SSL Certificate
+- (Optional) Domain Name and SSL Certificate (in `/certs` as `domain.pem` and `domain.key`)
 
 For a basic setup:
 
 ```bash
-docker compose up --detach
+sudo bash entrypoint.sh start
+
+# with SSL
+sudo bash entrypoint.sh start --ssl
+
+# with SSL and authentication
+sudo bash entrypoint.sh start --ssl --auth
 ```
+
+To stop the registry:
+
+```bash
+sudo bash entrypoint.sh stop
+```
+
+## Configuration
+
+Most of these configurations are automatically set by the `entrypoint.sh` script, but you can customize them by modifying the `docker-compose.yaml` file.
 
 ### Adding SSL
 
-If you want to expose the registry to the internet, you'll need to setup SSL to encrypt the connection between the client and the registry. To do this, add an SSL certificate and private key to the `./certs` directory.
-
 > [!WARNING]
-> The docker compose below assumes that the certificate is named `domain.crt` and the private key is named `domain.key`. If you have different names, you'll need to update the [docker-compose.ssl.yaml](./docker-compose.ssl.yaml) file.
+> The `entrypoint.sh` script assumes that the certificate is named `domain.pem` and the private key is named `domain.key`. If you have different names, you'll need to update the script.
 
-```bash
-docker compose -f docker-compose.ssl.yaml up --detach
-```
+If you want to expose the registry to the internet, you'll need to setup SSL to encrypt the connection between the client and the registry. To do this, add an SSL certificate and private key to the `./certs` directory.
 
 ### Adding Authentication
 
@@ -36,15 +48,10 @@ docker compose -f docker-compose.ssl.yaml up --detach
 
 By default, anyone can push and pull images from the registry. Authentication can be added to restrict access to the registry and mitigate against unauthorized access or certain denial-of-service attacks (i.e. filling up the registry with junk images).
 
-To add authentication, you'll need to create a htpasswd file with the following command:
+To add authentication, you can run the `entrypoint.sh` script with the `--auth` flag. This will start the registry with basic authentication. The default username and password are `admin` and `admin`. Under the hood, the script creates a htpasswd file with the username and password you specify using the `htpasswd` docker image:
+
 
 ```bash
-# create directory that will store the htpasswd file
-mkdir -p ./auth
-
 # create credentials for the registry
 docker run --rm --entrypoint htpasswd httpd:2 -Bbn testuser testpassword > auth/htpasswd
-
-# start the registry with authentication
-docker compose -f docker-compose.ssl-auth.yaml up --detach
 ```
---
+++ registry/entrypoint.sh
@@ -0,0 +1,139 @@
+#!/bin/env bash
+
+## CONSTANTS
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CERT_DIR=${CURRENT_DIR}/certs
+AUTH_DIR=${CURRENT_DIR}/auth
+STORAGE_DIR=${CURRENT_DIR}/storage
+
+APPLICATION_NAME=registry
+
+REGISRTY_IMAGE=registry:2
+REGISTRY_CONTAINER_NAME=registry
+REGISTRY_STORAGE_PATH=/var/lib/registry
+REGISRTY_CERT_PATH=/certs
+REGISRTY_AUTH_PATH=/auth
+REGISRTY_HTTP_PORT=5080
+REGISTRY_HTTPS_PORT=443 #5443
+
+HTPASSWD_IMAGE=httpd:2
+
+source $CURRENT_DIR/../utils.sh
+
+## FUNCTIONS
+
+usage () {
+  echo -e "\nUsage: $0 <command> [options]"
+  
+  echo -e "\nCommands:"
+  echo "  -> start: Start the registry"
+  echo "      --ssl     Start the registry in SSL mode"
+  echo "      --auth    Start the registry in Auth mode (with SSL)"
+  echo "  -> stop: Stop the registry"
+  exit 1
+}
+
+set_auth () {
+  log "Setting up authentication"
+  read -p "Username (leave empty for default - admin): " USERNAME
+  read -sp "Password (leave empty for default - admin): " PASSWORD
+
+  if [[ -z ${USERNAME} ]]; then
+    log "No username provided, using default credentials (admin/admin)"
+    
+    USERNAME="admin"
+    PASSWORD="admin"
+  fi
+
+  docker run --rm --entrypoint htpasswd ${HTPASSWD_IMAGE} -Bbn ${USERNAME} ${PASSWORD} > ${AUTH_DIR}/htpasswd
+}
+
+init_dirs () {
+  mkdir -p ${CERT_DIR}
+  mkdir -p ${AUTH_DIR}
+  mkdir -p ${STORAGE_DIR}
+}
+
+start_registry () {
+  MODE=$1
+  
+  init_dirs
+
+  if [[ -n "$MODE" && "$MODE" != "--ssl" && "$MODE" != "--auth" ]]; then
+    echo -e "Unknown option: $MODE"
+    usage
+  fi
+
+  if [[ "$MODE" == "--ssl" ]]; then
+    log "Starting the ${APPLICATION_NAME} in SSL mode"
+
+    docker run --detach --rm \
+      -p ${REGISTRY_HTTPS_PORT}:443 \
+      -e REGISTRY_HTTP_ADDR="0.0.0.0:443" \
+      -e REGISTRY_HTTP_TLS_CERTIFICATE=${REGISRTY_CERT_PATH}/domain.pem \
+      -e REGISTRY_HTTP_TLS_KEY=${REGISRTY_CERT_PATH}/domain.key \
+      -v ${CERT_DIR}:${REGISRTY_CERT_PATH} \
+      -v ${STORAGE_DIR}:${REGISTRY_STORAGE_PATH} \
+      --name ${REGISTRY_CONTAINER_NAME} \
+      ${REGISRTY_IMAGE}
+
+    log "Registry started on port ${REGISTRY_HTTPS_PORT}"
+    
+  elif [[ "$MODE" == "--auth" ]]; then
+    log "Starting the ${APPLICATION_NAME} in Auth mode"
+
+    set_auth
+
+    docker run --detach --rm \
+      -p ${REGISTRY_HTTPS_PORT}:443 \
+      -e REGISTRY_AUTH=htpasswd \
+      -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" \
+      -e REGISTRY_AUTH_HTPASSWD_PATH=${REGISRTY_AUTH_PATH}/htpasswd \
+      -e REGISTRY_HTTP_ADDR="0.0.0.0:443" \
+      -e REGISTRY_HTTP_TLS_CERTIFICATE=${REGISRTY_CERT_PATH}/domain.pem \
+      -e REGISTRY_HTTP_TLS_KEY=${REGISRTY_CERT_PATH}/domain.key \
+      -v ${CERT_DIR}:${REGISRTY_CERT_PATH} \
+      -v ${AUTH_DIR}:${REGISRTY_AUTH_PATH} \
+      -v ${STORAGE_DIR}:${REGISTRY_STORAGE_PATH} \
+      --name ${REGISTRY_CONTAINER_NAME} \
+      ${REGISRTY_IMAGE}
+
+    log "Registry started on port ${REGISTRY_HTTPS_PORT}"
+
+  else
+    log "Starting the ${APPLICATION_NAME} in normal mode"
+    
+    docker run --detach --rm \
+      -p ${REGISRTY_HTTP_PORT}:5000 \
+      -v ${STORAGE_DIR}:${REGISTRY_STORAGE_PATH} \
+      --name ${REGISTRY_CONTAINER_NAME} \
+      ${REGISRTY_IMAGE}
+
+    log "Registry started on port ${REGISRTY_HTTP_PORT}"
+  fi
+
+}
+
+stop_registry () {
+  log "Stopping the ${APPLICATION_NAME}"
+  docker stop ${REGISTRY_CONTAINER_NAME}
+}
+
+## MAIN
+
+check_deps "docker"
+
+case $1 in
+  "start")
+    start_registry $2
+    ;;
+  "stop")
+    stop_registry
+    ;;
+  *)
+    echo -e "Unknown command: $1"
+    usage
+    exit 1
+    ;;
+esac
