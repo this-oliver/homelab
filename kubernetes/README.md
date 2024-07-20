2a91f48f266e3611675496bab1315c4ed8ae78ca
oliver <olivermanzi.dev@gmail.com>
2024-07-20 22:09:34 +0000

    patch: (k8) updates setup script

--- kubernetes/README.md
+++ kubernetes/README.md
@@ -1,4 +1,4 @@
-# MicroK8
+# Kubernetes
 
 All applications hosted on the `homelab` are deployed on a Kubernetes cluster. The cluster is managed by [MicroK8s](https://microk8s.io/), a lightweight Kubernetes distribution that runs on a single node. MicroK8s is a great choice for homelabs because it is easy to install and manage, and it is resource-efficient.
 
@@ -9,16 +9,19 @@ Pre-requisites:
 - Ubuntu Operating System
 
 ```bash
-# script that installs microk8s
-bash entrypoint.sh
+# script that installs kubernetes
+bash entrypoint.sh setup
+
+# script that uninstall kubernetes
+bash entrypoint.sh teardown
+
+# open the kubernetes dashboard
+microk8s dashboard-proxy
 ```
 
 ## Usage
 
 ```bash
-# open the dashboard
-microk8s dashboard-proxy
-
 # deploy an application to the cluster
 microk8s kubectl apply -k /path/to/your/kustomization.yaml
 ```
--- kubernetes/entrypoint.sh
+++ kubernetes/entrypoint.sh
@@ -3,17 +3,24 @@
 # This script updates some firmware settings and installs microk8s on a
 # raspberry pi.
 
-_MODE=$1
-
 # == CONSTANTS ================================================================
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ALIAS_HELM="alias helm='microk8s helm3'"
 ALIAS_KUBERNETES="alias k8='microk8s kubectl'"
 ALIAS_MICROK8S="alias micro='microk8s'"
-BOOT_FILE=/boot/firmware/cmdline.txt
+BOOT_FILE_PATH=/boot/firmware/cmdline.txt
 BOOT_INSERT="cgroup_enable=memory cgroup_memory=1"
+DOCKER_REGISTRY_SECRET_NAME="dockerhub"
+DOCKER_DEFAULT_REGISTRY="https://index.docker.io/v1/"
 CERT_MANAGER_NAME="cert-manager"
 INGRESS_CONTROLLER_NAME="ingress-nginx"
+MICROK8S_ADDONS=(
+  "dns"
+  "helm"
+  "dashboard"
+)
 ORIGIN_CA_ISSUER_NAME="prod-issuer"
 ORIGIN_CA_ISSUER_SECRET_NAME="cloudflare-ca-key"
 ORIGIN_CA_ISSUER_RESOURCES=(
@@ -28,146 +35,127 @@ ORIGIN_CA_ISSUER_RESOURCES=(
   )
 ORIGIN_CA_ISSUER_VERSION="v0.9.0"
 
+source $CURRENT_DIR/../utils.sh
+
 # == FUNCTIONS ================================================================
 
-function usage {
-  echo "Usage: $0 <MODE>"
-  echo "  MODE      - setup or teardown (default: setup)"
+usage() {
+  echo -e "\nUsage: $0 <command>"
+  echo -e "\nCommands:"
+  echo "  setup     - setup microk8s"
+  echo "  teardown  - teardown microk8s"
   exit 1
 }
 
-# adds an alias string to the .bashrc file if it is not already there,
-# otherwise does nothing
-function add_alias {
-  ALIAS=$1
-
-  if ! grep -q "$ALIAS" ~/.bashrc; then
-    echo " ==== [MicroK8s Install] Adding '$ALIAS' to ~/.bashrc"
-    echo "$ALIAS" >> ~/.bashrc
-    source ~/.bashrc
-  fi
-}
-
-# prompts user for docker credentials and sets up a registry secret in kubernetes
-function configure_docker_credentials {
-  read -p "Do you want to setup docker credentials? [y/N] `echo $'\n> '`" SETUP_DOCKER_CREDENTIALS
-
-  if [[ $SETUP_DOCKER_CREDENTIALS =~ ^[Yy]$ ]]; then
-    read -p "Enter your docker username: `echo $'\n> '`" DOCKER_USERNAME
-    read -s -p "Enter your docker password: `echo $'\n> '`" DOCKER_PASSWORD
-    
-    echo -e "\n==== [MicroK8s Install] Setting up docker credentials for $DOCKER_USERNAME"
-    
-    microk8s kubectl create secret docker-registry dockerhub \
-      --docker-server="https://index.docker.io/v1/" \
-      --docker-username="$DOCKER_USERNAME" \
-      --docker-password="$DOCKER_PASSWORD"
-  fi
-}
-
-# installs snap if not already installed, otherwise does nothing
-function install_snap {
+# installs snap if not already installed
+install_snap() {
   # install snap if not installed (see https://snapcraft.io/docs/installing-snap-on-ubuntu)
   if [ -z "$(which snap)" ]; then
-      echo " ==== [MicroK8s Install] Installing snap"
-      sudo apt install snapd -y
+      log "[Kubernetes] Installing snap..."
+      apt install -y snapd
+      log "[Kubernetes] Successfully installed snap"
   fi
 }
 
 # installs microk8s, adds boot insert, enables some addons and sets up some aliases
-function install_microk8s {
-  if ! grep -q "$BOOT_INSERT" $BOOT_FILE; then
-      echo " ==== [MicroK8s Install] Adding '$BOOT_INSERT' to $BOOT_FILE"
-      sed -i "1s/^/$BOOT_INSERT /" $BOOT_FILE
+install_microk8s() {
+  if ! grep -q "$BOOT_INSERT" $BOOT_FILE_PATH; then
+      log "[Kubernetes] Adding '$BOOT_INSERT' to $BOOT_FILE_PATH (requires reboot)..."
+      sed -i "1s/^/$BOOT_INSERT /" $BOOT_FILE_PATH
   fi
 
   if [ -z "$(which microk8s)" ]; then
     # uninstall old versions of microk8s to avoid conflicts
-    echo " ==== [MicroK8s Install] Uninstalling old versions of microk8s"
-    sudo snap remove microk8s
+    log "[Kubernetes] Uninstalling old versions of microk8s..."
+    snap remove microk8s
 
     # install microk8s (see https://microk8s.io/docs/install-raspberry-pi#installation)
-    echo " ==== [MicroK8s Install] Installing microk8s"
-    sudo apt install linux-modules-extra-raspi -y
-    sudo snap install microk8s --classic --channel=1.30
-
-    # enable some addons
-    echo " ==== [MicroK8s Install] Enabling microk8s addons"
-    microk8s enable dns helm dashboard
-
-    # add aliases
-    add_alias "$ALIAS_MICROK8S"
-    add_alias "$ALIAS_KUBERNETES"
-    add_alias "$ALIAS_HELM"
+    log "[Kubernetes] Installing microk8s..."
+    apt install -y linux-modules-extra-raspi
+    snap install microk8s --classic --channel=1.30
+  else
+    log "[Kubernetes] microk8s already installed. Updating instead..."
+    snap refresh microk8s --classic --channel=1.30
   fi
+
+  # enable some addons
+  log "[Kubernetes] Enabling microk8s addons ${MICROK8S_ADDONS[@]}..."
+  for add_on in "${MICROK8S_ADDONS[@]}"; do
+    microk8s enable $add_on
+  done
+
+  # add aliases
+  log "[Kubernetes] Adding aliases..."
+  add_alias "$ALIAS_MICROK8S"
+  add_alias "$ALIAS_KUBERNETES"
+  add_alias "$ALIAS_HELM"
 }
 
-function uninstall_microk8s {
-  echo " ==== [MicroK8s Install] Uninstalling microk8s"
-  sudo snap remove microk8s
+uninstall_microk8s() {
+  log "[Kubernetes] Uninstalling microk8s..."
+  snap remove microk8s
 
-  if grep -q "$BOOT_INSERT" $BOOT_FILE; then
-      echo " ==== [MicroK8s Install] Removing '$BOOT_INSERT' from $BOOT_FILE"
-      sed -i "s/$BOOT_INSERT //" $BOOT_FILE
+  if grep -q "$BOOT_INSERT" $BOOT_FILE_PATH; then
+      log "[Kubernetes] Removing '$BOOT_INSERT' from $BOOT_FILE_PATH"
+      sed -i "s/$BOOT_INSERT //" $BOOT_FILE_PATH
   fi
 
-  echo " ==== [MicroK8s Install] Removing aliases from ~/.bashrc"
-  sed -i "/$ALIAS_MICROK8S/d" ~/.bashrc
-  sed -i "/$ALIAS_KUBERNETES/d" ~/.bashrc
-  sed -i "/$ALIAS_HELM/d" ~/.bashrc
+  log "[Kubernetes] Removing aliases..."
+  remove_alias "$ALIAS_MICROK8S"
+  remove_alias "$ALIAS_KUBERNETES"
+  remove_alias "$ALIAS_HELM"
 }
 
 # installs cert-manager + cloudflare's issuer for issuing certificates to
 # applications deployed on the cluster via the cloudflare api
-function install_cert_manager {
+install_cert_manager() {
   ISSUER_TEMP_YAML=$(mktemp)
   
   # [!NOTE] see https://cert-manager.io/docs/installation/helm/
   if [ -z "$(microk8s helm3 repo list | grep jetstack)" ]; then
-    echo " ==== [MicroK8s Install] Installing cert-manager"
-    
+    log "[Kubernetes] Installing cert-manager..."
+
     microk8s helm3 repo add jetstack https://charts.jetstack.io --force-update
     microk8s helm3 install cert-manager jetstack/cert-manager --namespace "$CERT_MANAGER_NAME" --create-namespace --version v1.15.1 --set crds.enabled=true
   fi
-  
-  echo " ==== [MicroK8s Install] Installing cloudflare crds, rbac and manifest"
 
   # apply each resource
+  log "[Kubernetes] Installing cloudflare origin ca issuer..."
   for RESOURCE in "${ORIGIN_CA_ISSUER_RESOURCES[@]}"; do
     microk8s kubectl apply -f https://raw.githubusercontent.com/cloudflare/origin-ca-issuer/${ORIGIN_CA_ISSUER_VERSION}/deploy/$RESOURCE
   done
   
-  echo " ==== [MicroK8s Install] Setting up cloudflare origin ca key and origin issuer"
-  read -s -p "Enter your cloudflare origin ca key (see https://dash.cloudflare.com/profile/api-tokens): `echo $'\n> '`" CLOUDFLARE_CA_KEY
+  log "[Kubernetes] Setting up cloudflare origin ca issuer..."
+  CLOUDFLARE_CA_KEY="$(prompt "Enter your cloudflare origin ca key (see https://dash.cloudflare.com/profile/api-tokens):" --secret)"
   
   # prepare origin issuer yaml
   echo "
   apiVersion: cert-manager.k8s.cloudflare.com/v1
   kind: OriginIssuer
   metadata:
-    name: "$ORIGIN_CA_ISSUER_NAME"
+    name: "${ORIGIN_CA_ISSUER_NAME}"
     namespace: default
   spec:
     requestType: OriginECC
     auth:
       serviceKeyRef:
-        name: "$ORIGIN_CA_ISSUER_SECRET_NAME"
+        name: "${ORIGIN_CA_ISSUER_SECRET_NAME}"
         key: key
   " > $ISSUER_TEMP_YAML
 
   # remove old secret
-  microk8s kubectl delete secret --ignore-not-found -n default "$ORIGIN_CA_ISSUER_SECRET_NAME"
+  microk8s kubectl delete secret --ignore-not-found -n default "${ORIGIN_CA_ISSUER_SECRET_NAME}"
 
   # [!NOTE] see https://github.com/cloudflare/origin-ca-issuer
-  microk8s kubectl create secret generic -n default "$ORIGIN_CA_ISSUER_SECRET_NAME" --from-literal key="$CLOUDFLARE_CA_KEY"
+  microk8s kubectl create secret generic -n default "${ORIGIN_CA_ISSUER_SECRET_NAME}" --from-literal key="${CLOUDFLARE_CA_KEY}"
   microk8s kubectl apply -f $ISSUER_TEMP_YAML
 
   # remove the temporary file
   rm $ISSUER_TEMP_YAML
 }
 
-function uninstall_cert_manager {
-  echo " ==== [MicroK8s Install] Uninstalling cert-manager"
+uninstall_cert_manager() {
+  log "[Kubernetes] Uninstalling cert-manager..."
   microk8s helm3 uninstall cert-manager -n cert-manager
 
   for RESOURCE in "${ORIGIN_CA_ISSUER_RESOURCES[@]}"; do
@@ -180,47 +168,74 @@ function uninstall_cert_manager {
 
 # installs an ingress controller that routes traffic to services in the cluster
 # depending on the host and path of the request (requires cert-manager for tls)
-function install_ingress_controller {
-  echo " ==== [MicroK8s Install] Installing ingress controller"
+install_ingress_controller() {
+  log "[Kubernetes] Installing ingress controller..."
 
   microk8s helm3 upgrade --install ingress-nginx ingress-nginx \
     --repo https://kubernetes.github.io/ingress-nginx \
     --namespace "$INGRESS_CONTROLLER_NAME" --create-namespace
 }
 
-function uninstall_ingress_controller {
-  echo " ==== [MicroK8s Install] Uninstalling ingress controller"
+uninstall_ingress_controller() {
+  log "[Kubernetes] Uninstalling ingress controller..."
   microk8s helm3 uninstall ingress-nginx -n "$INGRESS_CONTROLLER_NAME"
 }
 
-function setup {
-  echo " ==== [MicroK8s Install] Updating system"
-  sudo apt update
-  
+# prompts user for docker credentials and sets up a registry secret in kubernetes
+configure_docker_credentials() {
+  log "[Kubernetes] Setting up docker-registry secret \"${DOCKER_REGISTRY_SECRET_NAME}\" in the default namespace..."
+  SETUP_DOCKER_CREDENTIALS="$(prompt "Do you want to setup docker credentials? [y/N]")"
+
+  if [[ $SETUP_DOCKER_CREDENTIALS =~ ^[Yy]$ ]]; then
+    DOCKER_REGISTRY="$(prompt "Enter your docker registry (leave empty for \"${DOCKER_DEFAULT_REGISTRY}\"):")"
+    DOCKER_USERNAME="$(prompt "Enter your docker username:")"
+    DOCKER_PASSWORD="$(prompt "Enter your docker password:" --secret)"
+
+    if [ -z "$DOCKER_REGISTRY" ]; then
+      DOCKER_REGISTRY="${DOCKER_DEFAULT_REGISTRY}"
+    fi
+
+    microk8s kubectl create secret docker-registry ${DOCKER_REGISTRY_SECRET_NAME} \
+      --docker-server="${DOCKER_REGISTRY}" \
+      --docker-username="${DOCKER_USERNAME}" \
+      --docker-password="${DOCKER_PASSWORD}"
+  fi
+}
+
+setup() {
+  check_sudo
+  apt update
   install_snap
   install_microk8s
   install_cert_manager
   install_ingress_controller
   configure_docker_credentials
-  echo " ==== [MicroK8s Install] Microk8s installed!"
+  log "[MicroK8s] Setup complete!"
 }
 
-function teardown {
+teardown() {
   uninstall_microk8s
   uninstall_cert_manager
   uninstall_ingress_controller
-  echo " ==== [MicroK8s Install] Microk8s uninstalled!"
+  log "[MicroK8s] Teardown complete!"
 }
 
 # == SCRIPTS ==================================================================
 
-if [ -z "$_MODE" ]; then
-  echo " ==== [MicroK8s Install] Setting up microk8s"
-  setup
-elif [ "$_MODE" == "teardown" ]; then
-  echo " ==== [MicroK8s Install] Tearing down microk8s"
-  teardown
-else
-  usage
-  exit 1
-fi
+case $1 in
+  setup)
+    setup
+
+    # run uninstall if setup fails
+    if [ $? -ne 0 ]; then
+      log ERROR "[Kubernetes] Setup failed. Running teardown..."
+      teardown
+    fi
+    ;;
+  teardown)
+    teardown
+    ;;
+  *)
+    usage
+    ;;
+esac
